c1d87ebee2838ebb59dabe91929205b6f2331513
kanekotic <me@kanekotic.com>
2021-04-25 18:20:13 +0200

    #3 | Alvaro | allow retrival of quote by id

--- src/repository/quotes.ts
+++ src/repository/quotes.ts
@@ -1,6 +1,12 @@
 import * as data from '../../quotes.json';
 import { quote } from '../types/quotes';
 
-export const getQuotesForUser = (_: string): quote[] => {
-  return data;
-};
+export const getQuotesForUser = (_: string): quote[] => data as quote[];
+
+export const getQuoteForUserById = (_: string, id: string): quote =>{
+  const quote = (data as quote[]).find((quote) => quote.id === id);
+  if(!quote)
+    throw new Error("Unable to find quote with id");
+  return quote;
+}
+  
--- test/repository/quotes.test.ts
+++ test/repository/quotes.test.ts
@@ -1,9 +1,24 @@
-import { getQuotesForUser } from '../../src/repository/quotes';
+import {
+  getQuoteForUserById,
+  getQuotesForUser,
+} from '../../src/repository/quotes';
+import * as faker from 'faker';
 import * as data from '../../quotes.json';
 describe('quotes repository should', () => {
   describe('getQuotesForUser should', () => {
     test('return quotes for user', () => {
       expect(getQuotesForUser('')).toEqual(data);
+    });
+  });
+  describe('getQuoteForUserById should', () => {
+    const randomIndex = Math.floor(Math.random() * 31);
+    test('return quotes for user', () => {
+      expect(getQuoteForUserById('', data[randomIndex].id)).toEqual(
+        data[randomIndex]
+      );
+    });
+    test('throw if id does not exist', () => {
+      expect(() => getQuoteForUserById('', faker.random.word())).toThrow();
     });
   });
 });
