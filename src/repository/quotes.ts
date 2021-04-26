0c3f5a36d8c897bfeae5a32cf002d6496adcec22
kanekotic <me@kanekotic.com>
2021-04-26 09:50:59 +0200

    NOISSUE | Alvaro | add store for quotes based on unique existing user

--- src/repository/quotes.ts
+++ src/repository/quotes.ts
@@ -1,10 +1,18 @@
 import * as data from '../../quotes.json';
 import { quote } from '../types/quotes';
+export const _ = {
+  userQuotesStore: {
+    [process.env.USERNAME]: data,
+  },
+};
+export const getQuotesForUser = (user: string): quote[] => {
+  const quotes = _.userQuotesStore[user];
+  if (!quotes) throw new Error('No quotes for request user');
+  return quotes;
+};
 
-export const getQuotesForUser = (_: string): quote[] => data as quote[];
-
-export const getQuoteForUserById = (_: string, id: string): quote => {
-  const quote = (data as quote[]).find((quote) => quote.id === id);
+export const getQuoteForUserById = (user: string, id: string): quote => {
+  const quote = getQuotesForUser(user).find((quote) => quote.id === id);
   if (!quote) throw new Error('Unable to find quote with id');
   return quote;
 };
--- test/repository/quotes.test.ts
+++ test/repository/quotes.test.ts
@@ -6,19 +6,29 @@ import * as faker from 'faker';
 import * as data from '../../quotes.json';
 describe('quotes repository should', () => {
   describe('getQuotesForUser should', () => {
-    test('return quotes for user', () => {
-      expect(getQuotesForUser('')).toEqual(data);
+    test('return quotes for existing user', () => {
+      expect(getQuotesForUser(process.env.USERNAME)).toEqual(data);
+    });
+    test('throws for non existing user', () => {
+      expect(() => getQuotesForUser(faker.random.word())).toThrow();
     });
   });
   describe('getQuoteForUserById should', () => {
     const randomIndex = Math.floor(Math.random() * 31);
     test('return quotes for user', () => {
-      expect(getQuoteForUserById('', data[randomIndex].id)).toEqual(
-        data[randomIndex]
-      );
+      expect(
+        getQuoteForUserById(process.env.USERNAME, data[randomIndex].id)
+      ).toEqual(data[randomIndex]);
+    });
+    test('throw if id user does not exist', () => {
+      expect(() =>
+        getQuoteForUserById(faker.random.word(), data[randomIndex].id)
+      ).toThrow();
     });
     test('throw if id does not exist', () => {
-      expect(() => getQuoteForUserById('', faker.random.word())).toThrow();
+      expect(() =>
+        getQuoteForUserById(process.env.USERNAME, faker.random.word())
+      ).toThrow();
     });
   });
 });
